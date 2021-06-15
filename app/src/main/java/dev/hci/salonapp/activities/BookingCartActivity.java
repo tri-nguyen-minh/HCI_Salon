214cfa2689e506e93d50f087750626db72ec7481
Tri Nguyen <trimn49@gmail.com>
2021-06-16 01:57:05 +0700

    Date

--- app/src/main/java/dev/hci/salonapp/activities/BookingCartActivity.java
+++ app/src/main/java/dev/hci/salonapp/activities/BookingCartActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import dev.hci.salonapp.R;
 
@@ -13,6 +17,14 @@ public class BookingCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_cart);
+
+        CalendarView calendarView = findViewById(R.id.calViewPicker);
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendarView.setMinDate((new Date().getTime()));
+        calendarView.setSelectedWeekBackgroundColor(getResources().getColor(R.color.gold));
+
     }
 
     public void onBackBooking(View view) {
