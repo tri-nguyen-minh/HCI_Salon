5d90309922269eda47536d3c993c63282b480a74
TriNguyen140329 <laica97@gmail.com>
2021-06-16 02:38:40 +0700

    Booking Page

--- app/src/main/java/dev/hci/salonapp/activities/BookingCartActivity.java
+++ app/src/main/java/dev/hci/salonapp/activities/BookingCartActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 import dev.hci.salonapp.R;
 
 public class BookingCartActivity extends AppCompatActivity {
+
+    private Integer[] timeslotIdList = {R.id.timeslot08, R.id.timeslot09,
+                                        R.id.timeslot10, R.id.timeslot11,
+                                        R.id.timeslot12, R.id.timeslot13,
+                                        R.id.timeslot14, R.id.timeslot15,
+                                        R.id.timeslot16, R.id.timeslot17,
+                                        R.id.timeslot18, R.id.timeslot19,
+                                        R.id.timeslot20};
+    private TextView textViewCommon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +40,18 @@ public class BookingCartActivity extends AppCompatActivity {
 
     public void onBackBooking(View view) {
         finish();
+    }
+
+    public void onTimeslotClick(View view) {
+        resetTimeslot();
+        textViewCommon = findViewById(view.getId());
+        textViewCommon.setBackground(getResources().getDrawable(R.drawable.background_timeslot_selected));
+    }
+
+    private void resetTimeslot() {
+        for (int slot : timeslotIdList) {
+            textViewCommon = findViewById(slot);
+            textViewCommon.setBackground(getResources().getDrawable(R.drawable.background_like));
+        }
     }
 }
