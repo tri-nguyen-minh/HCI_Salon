0e24ad8a2c8cb16dfd9a86d08298b13138adea30
TriNguyen140329 <laica97@gmail.com>
2021-06-27 23:39:10 +0700

    hour

--- app/src/main/java/dev/hci/manager/fragments/schedule/GeneralScheduleFragment.java
+++ app/src/main/java/dev/hci/manager/fragments/schedule/GeneralScheduleFragment.java
@@ -1,16 +1,19 @@
 package dev.hci.manager.fragments.schedule;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.hci_salon_manager.R;
 
 public class GeneralScheduleFragment extends Fragment {
+
+    private TextView textViewCommon;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,12 @@ public class GeneralScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule_1_general, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        EditText txtTimeFrom = getView().findViewById(R.id.txtTimeFrom);
+        txtTimeFrom.setOnEditorActionListener();
     }
 }
