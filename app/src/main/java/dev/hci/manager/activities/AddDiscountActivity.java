366752bd07bac3dba0d496cc0d60070f6567b3e7
tri-nguyen-minh <trimn49@gmail.com>
2021-07-07 18:20:41 +0700

    discount

---
+++ app/src/main/java/dev/hci/manager/activities/AddDiscountActivity.java
@@ -0,0 +1,86 @@
+package dev.hci.manager.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import dev.hci.manager.R;
+import dev.hci.manager.dtos.ServiceDetail;
+import dev.hci.manager.recycleviewadapter.RecViewServiceAdapter;
+
+public class AddDiscountActivity extends AppCompatActivity {
+
+    private ArrayList<ServiceDetail> serviceDetailsList;
+    private Intent intent;
+    private ServiceDetail discount;
+    private EditText editTitle, editValue;
+    private RecyclerView recViewDiscountService;
+    private RecViewServiceAdapter adapter;
+    private int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_discount);
+
+        intent = getIntent();
+        position = intent.getIntExtra("POSITION", -1);
+        setUpList();
+
+        serviceDetailsList = new ArrayList<>();
+        serviceDetailsList.add(new ServiceDetail("Male Haircut", "30 - 40 minutes", "50", "0",35,0));
+        serviceDetailsList.add(new ServiceDetail("Female Haircut", "30 - 45 minutes", "100", "0",61,0));
+        serviceDetailsList.add(new ServiceDetail("Hair Styling", "30 minutes", "70", "0",87,0));
+        serviceDetailsList.add(new ServiceDetail("Hair Loss Treatment", "60 - 120 minutes", "840", "1,200",6,30));
+        serviceDetailsList.add(new ServiceDetail("Hair Collagen Treatment", "30 - 60 minutes", "270", "450",32,40));
+        serviceDetailsList.add(new ServiceDetail("Hair Keratin Treatment", "30 - 60 minutes", "350", "500",29,30));
+        serviceDetailsList.add(new ServiceDetail("Hair Wash", "15 minutes", "30", "0",91,0));
+        serviceDetailsList.add(new ServiceDetail("Wash + Massage", "60 minutes", "42", "70",75,40));
+        serviceDetailsList.add(new ServiceDetail("Coloring (Short)", "60 - 90 minutes", "550", "0",24,0));
+        serviceDetailsList.add(new ServiceDetail("Coloring (Long)", "60 - 90 minutes", "490", "700",45,30));
+        serviceDetailsList.add(new ServiceDetail("Hair Curling", "30 - 60 minutes", "400", "0",21,0));
+        serviceDetailsList.add(new ServiceDetail("Hair Straightening", "60 - 120 minutes", "450", "0",23,0));
+
+        int count = 0;
+        for (int i = 0; i < discount.getList().size(); i++) {
+            for (int j = 0; j < serviceDetailsList.size(); j++) {
+                if (discount.getList().get(i).getName().equals(serviceDetailsList.get(j).getName())) {
+                    serviceDetailsList.get(j).setDiscounted(true);
+                }
+            }
+        }
+
+        setupRecycleView(serviceDetailsList);
+
+
+        this.getSupportActionBar().hide();
+    }
+
+    private void setupRecycleView(ArrayList<ServiceDetail> list) {
+        recViewDiscountService = findViewById(R.id.recViewAddDiscountService);
+        adapter = new RecViewServiceAdapter(AddDiscountActivity.this, AddDiscountActivity.this, 4);
+        adapter.setServiceDetailsList(list);
+        recViewDiscountService.setAdapter(adapter);
+        recViewDiscountService.setLayoutManager(new LinearLayoutManager(AddDiscountActivity.this, RecyclerView.VERTICAL,false));
+    }
+
+    private void setUpList() {
+        discount = (ServiceDetail) intent.getSerializableExtra("DISCOUNT");
+    }
+
+    public void onBackDiscount(View view) {
+        setUpList();
+        intent = new Intent(getApplicationContext(), EditDiscountActivity.class);
+        intent.putExtra("DISCOUNT", discount);
+        intent.putExtra("POSITION", position);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/dev/hci/manager/activities/EditDiscountActivity.java
@@ -0,0 +1,187 @@
+package dev.hci.manager.activities;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import dev.hci.manager.R;
+import dev.hci.manager.dtos.ServiceDetail;
+import dev.hci.manager.recycleviewadapter.RecViewServiceAdapter;
+
+public class EditDiscountActivity extends AppCompatActivity {
+
+    private Intent intent;
+    private ServiceDetail discount;
+    private EditText editTitle, editValue;
+    private RecyclerView recViewDiscountService;
+    private RecViewServiceAdapter adapter;
+    private int position;
+    private DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_discount);
+        decimalFormat.setGroupingUsed(true);
+        decimalFormat.setGroupingSize(3);
+
+        intent = getIntent();
+        position = intent.getIntExtra("POSITION", -1);
+        setUpList();
+        editTitle = findViewById(R.id.editDiscountName);
+        editValue = findViewById(R.id.editDiscountValue);
+
+        if (discount != null) {
+            editTitle.setText(discount.getName());
+            editValue.setText(discount.getDiscount() + "");
+            setupDiscount();
+            setupRecycleView(discount.getList());
+        }
+
+        editValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (discount != null) {
+                    setupDiscount();
+                    setupRecycleView(discount.getList());
+                }
+            }
+        });
+
+        this.getSupportActionBar().hide();
+    }
+
+    private void setUpList() {
+        discount = (ServiceDetail) intent.getSerializableExtra("DISCOUNT");
+    }
+
+    private void setupRecycleView(ArrayList<ServiceDetail> list) {
+        recViewDiscountService = findViewById(R.id.recViewDiscountService);
+        adapter = new RecViewServiceAdapter(EditDiscountActivity.this, EditDiscountActivity.this, 3);
+        adapter.setServiceDetailsList(list);
+        recViewDiscountService.setAdapter(adapter);
+        recViewDiscountService.setLayoutManager(new LinearLayoutManager(EditDiscountActivity.this, RecyclerView.VERTICAL,false));
+    }
+
+    public void onBackDiscount(View view) {
+        startActivity(new Intent(getApplicationContext(), DiscountActivity.class));
+    }
+
+    public void onRegisterClick(View view) {
+        if (discount == null) {
+            discount = new ServiceDetail();
+            discount.setList(new ArrayList<>());
+        }
+        discount.setName(editTitle.getText().toString());
+        setupDiscount();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditDiscountActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle("Confirm Discount Details");
+        builder.setMessage("Do you want to add this Discount?");
+        builder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        intent = new Intent(getApplicationContext(), DiscountActivity.class);
+                        intent.putExtra("DISCOUNT", discount);
+                        intent.putExtra("POSITION", position);
+                        startActivity(intent);
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void setupDiscount() {
+        int discountValue = 0, price = 0;
+        discount.setDiscount(checkDiscountValue());
+        for (ServiceDetail serviceDetail: discount.getList()) {
+            discountValue = checkDiscountValue() / 10;
+            if (serviceDetail.getDiscount() > 0) {
+                price = Integer.parseInt(checkPriceString(serviceDetail.getOrgPrice()));
+            } else {
+                price = Integer.parseInt(checkPriceString(serviceDetail.getPrice()));
+            }
+            serviceDetail.setOrgPrice(decimalFormat.format(price));
+            price = (price / 10) * (10 - discountValue);
+            serviceDetail.setPrice(price + "");
+            serviceDetail.setDiscount(checkDiscountValue());
+        }
+    }
+
+    public void onAddDiscountClick(View view) {
+
+        if (position >= 0) {
+            discount.setName(editTitle.getText().toString());
+            int discountValue = 0, price = 0;
+            discount.setDiscount(checkDiscountValue());
+            for (ServiceDetail serviceDetail: discount.getList()) {
+                discountValue = checkDiscountValue() / 10;
+                if (serviceDetail.getDiscount() > 0) {
+                    price = Integer.parseInt(checkPriceString(serviceDetail.getOrgPrice()));
+                } else {
+                    price = Integer.parseInt(checkPriceString(serviceDetail.getPrice()));
+                }
+                price = (price / 10) * (10 - discountValue);
+                serviceDetail.setPrice(price + "");
+                serviceDetail.setDiscount(discountValue);
+            }
+        }else {
+            discount = new ServiceDetail();
+            discount.setName(editTitle.getText().toString());
+            int price = checkDiscountValue();
+            discount.setDiscount(price);
+            discount.setList(new ArrayList<>());
+        }
+        intent = new Intent(getApplicationContext(), AddDiscountActivity.class);
+        intent.putExtra("DISCOUNT", discount);
+        intent.putExtra("POSITION", position);
+        startActivity(intent);
+    }
+
+    private int checkDiscountValue() {
+        return editValue.getText().toString().isEmpty() ? 0 : Integer.parseInt(editValue.getText().toString());
+    }
+
+
+    private String checkPriceString(String string) {
+        if (string.indexOf(",") >= 0) {
+            String start = string.substring(0, string.indexOf(","));
+            String end = string.substring(string.indexOf(",") + 1);
+            string =  start + end;
+        }
+        return string;
+    }
+}
